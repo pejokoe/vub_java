f845f8bc876d1c3e71d523a3c1d56d48fa6bb9c1
1822913 <peterkoegler@gmx.de>
2022-10-17 23:13:26 +0200

    TextManipulation complete, tests don't pass so further evaluation necessary

--- TextManipulation/src/TextManipulation.java
+++ TextManipulation/src/TextManipulation.java
@@ -5,15 +5,15 @@ public class TextManipulation {
 	public TextManipulation() {
 		return;
 	}
-	
+
 	public String convertToUpperCase(String s) {
 		return s.toUpperCase();
 	}
-	
+
 	public String convertToLowerCase(String s) {
 		return s.toLowerCase();
 	}
-	
+
 	public boolean isAnagram(String a, String b) {
 		a = a.toLowerCase();
 		b = b.toLowerCase();
@@ -22,23 +22,47 @@ public class TextManipulation {
 		Arrays.sort(tmp_a);
 		Arrays.sort(tmp_b);
 		return Arrays.equals(tmp_a, tmp_b);
-		
+
 	}
-	
-	public String reverseString(String a) {
+
+	public String reversString(String a) {
 		char[] tmp_a = a.toCharArray();
 		char[] tmp_b = new char[tmp_a.length];
-		for(int i = 0; i < tmp_a.length; i++) {
-			tmp_b[i] = tmp_a[tmp_a.length-1-i];
+		for (int i = 0; i < tmp_a.length; i++) {
+			tmp_b[i] = tmp_a[tmp_a.length - 1 - i];
 		}
 		a = new String(tmp_b);
 		return a;
 	}
-	
+
+	public String caesarCipherCode(String input, int cipherShift) {
+		char[] letters = input.toLowerCase().toCharArray();
+		if (cipherShift < 0)
+			cipherShift = 26 + cipherShift % 26;
+		for (int i = 0; i < letters.length; i++) {
+			if ('a' <= letters[i] && letters[i] <= 'z') {
+				int tmp = (((int) letters[i] - 97) + cipherShift) % 26;
+				letters[i] = (char) (tmp + 97);
+			}
+		}
+		return new String(letters);
+	}
+
+	public String caesarCipherDecode(String input, int cipherShift) {
+		return caesarCipherCode(input, -cipherShift);
+	}
+
 	public static void main(String[] args) {
 		TextManipulation test = new TextManipulation();
 		System.out.println(test.isAnagram("Peter", "ReetP"));
-		System.out.println(test.reverseString("gfedcba"));
-		
+		System.out.println(test.reversString("gfedcba"));
+		System.out.println(test.caesarCipherCode(
+				"Ftq cguow ndaiz raj\n"
+				+ "vgybe ahqd ftq xmlk pas.\n"
+				+ "Ftq ruhq najuzs\n"
+				+ "iulmdpe vgyb cguowxk.",
+				-12));
+//		System.out.println(test.caesarCipherDecode(test.caesarCipherCode("abcd", -60), -60));
+
 	}
 }
