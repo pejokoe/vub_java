e9f380737a1a3f5263a180b2e8c43639ea0dbe9c
Peter <1822913@stud.hs-mannheim.de>
2022-11-09 11:38:48 +0100

    live_testing and start of banking

--- Banking/src/BankAccount.java
+++ Banking/src/BankAccount.java
@@ -7,35 +7,44 @@ public class BankAccount {
         /***
         Constructor
         ***/
+    	this.uniqueID = uniqueID;
+    	this.balance = amount;
     }
 
     public void addToBalance(double amount){
         /***
          Add to an account
          ***/
+    	this.balance += amount;
     }
 
     public boolean canBeRemovedFromBalance(double amount){
         /***
          Check if the balance can be removed from an account
          ***/
+    	return this.balance - amount > 0;
     }
 
     public void removeFromBalance(double amount){
         /***
          Remove from an account if the conditions are valid
          ***/
+    	if (this.canBeRemovedFromBalance(amount)) {
+    		this.balance -= amount;
+    	}
     }
 
     public double getBalance() {
         /***
          Getter
          ***/
+    	return this.balance;
     }
 
     public int getID() {
         /***
          Getter
          ***/
+    	return this.uniqueID;
     }
 }
---
+++ live_testing/src/live_testing.java
@@ -0,0 +1,4 @@
+
+public class live_testing {
+
+}
