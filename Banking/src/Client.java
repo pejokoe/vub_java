df6ee53411d9a28ca9705e716698c56534a34da2
Peter <1822913@stud.hs-mannheim.de>
2022-11-09 11:58:59 +0100

    Start implementing client

--- Banking/src/Client.java
+++ Banking/src/Client.java
@@ -16,59 +16,72 @@ public class Client {
         /***
          Constructor
          ***/
+    	this.name = name;
+    	this.age = age;
+    	this.IDnumber = IDnumber;
+    	this.adress = adress;
     }
 
     public void addAccount(BankAccount bankAccount){
         /***
          Add an account to the user
          ***/
+    	this.accounts.add(bankAccount);
     }
 
     public void removeAccount(BankAccount selectedAccount){
         /***
          Remove an account from the user, think of edge cases. Can you always remove an account?
          ***/
+    	this.accounts.remove(selectedAccount);
     }
 
     public int getAge() {
         /***
          Getter
          ***/
+    	return this.age;
     }
 
     public String getName() {
         /***
          Getter
          ***/
+    	return this.name;
     }
 
     public List<BankAccount> getAccounts(){
         /***
          Getter
          ***/
+    	return this.accounts;
     }
 
     public void addBankID(int bankID) {
         /***
          Setter
          ***/
+    	this.bankID = bankID;
     }
 
     public int getBankID(){
         /***
          Getter
          ***/
+    	return this.bankID;
     }
 
     public void setPin(int pin){
         /***
          Setter
          ***/
+    	this.pin = pin;
     }
 
     public int getPin(){
         /***
          Getter
          ***/
+    	return this.pin;
     }
 }
