9ffbef0b2a289e213e896945361bef2dba3aae8b
1822913 <peterkoegler@gmx.de>
2022-11-25 13:00:20 +0100

    Main in extra module

---
+++ Library_System/src/program_library.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class program_library {
+
+	public static void main(String[] args) {
+		Library myLibrary = new Library();
+		System.out.println("Running test");
+		ArrayList<Book> HP = new ArrayList(Arrays.asList(new Book("Harry Potter 1"), new Book("Harry Potter 2"),
+				new Book("Harry Potter 3"), new Book("Harry Potter 4"), new Book("Harry Potter 5"),
+				new Book("Harry Potter 6"), new Book("Harry Potter 7")));
+
+		for (int i = 0; i < HP.size(); i++) {
+			myLibrary.addBook(HP.get(i), i + 1);
+		}
+		User Max = new User("Max");
+		User Frank = new User("Frank");
+
+		// Setup
+		myLibrary.addUser(Max);
+		myLibrary.addUser(Frank);
+		myLibrary.printBooks();
+
+		// Test checkout book
+		myLibrary.checkOutBook(Frank, HP.get(4));
+		myLibrary.printBooks();
+
+		User noMember = new User("Dummie");
+		myLibrary.checkOutBook(noMember, HP.get(4));
+		myLibrary.printBooks();
+
+		Book notInInventory = new Book("dummie");
+		myLibrary.checkOutBook(Frank, notInInventory);
+		myLibrary.printBooks();
+
+		myLibrary.checkOutBook(Frank, HP.get(0));
+		myLibrary.checkOutBook(Max, HP.get(0));
+
+		myLibrary.contactAllCheckedOut();
+		myLibrary.giveBack(Frank, HP.get(4));
+		myLibrary.contactAllCheckedOut();
+	}
+}
